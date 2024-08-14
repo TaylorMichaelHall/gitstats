004c9c5b86af557b0196a42a6910d13c8bfe0c80
Taylor Hall <taylormichaelhall@gmail.com>
2024-08-13 21:57:51 -0500

    fix commit count

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
+	"gitstats/cmd"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	"gitstats/cmd"
 )
 
 func main() {
 	var rootCmd = &cobra.Command{
 		Use:   "gitstats",
 		Short: "A tool for analyzing Git repository statistics",
-		Long: `gitstats is a CLI tool that provides various statistics about a Git repository.`,
+		Long:  `gitstats is a CLI tool that provides various statistics about a Git repository.`,
 	}
 
 	rootCmd.AddCommand(cmd.GetContributorsCmd(), cmd.GetFilesCmd())
